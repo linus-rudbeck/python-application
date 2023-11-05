3f17356ef557ccb98c85ae6118e4b24ff0c000d3
linus-rudbeck <linus.rudbeck@distansakademin.se>
2023-11-05 10:18:49 +0100

    Update test_hello_world.py

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,4 +1,7 @@
 from python_application.hello_world import get_greeting
 
-def test_main():
+def test_get_greeting():
     assert get_greeting() == 'Hello World!'
+
+def test_get_greeting_intentional_fail():
+    assert get_greeting() == 'abc123'
